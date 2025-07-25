666fa9bc971b7068590f4e1f257e93d21e956aca
Farrelias04 <wmredzafarrel@gmail.com>
2025-07-25 22:59:25 +0800

    Day 3: Add edit and delete functionality for expenses

--- app/routes.py
+++ app/routes.py
@@ -20,3 +20,23 @@ def index():
     
     expenses = Expense.query.all()
     return render_template('index.html', form=form, expenses=expenses)
+
+@main.route('/edit/<int:expense_id>', methods=['GET', 'POST'])
+def edit_expense(expense_id):
+    expense = Expense.query.get_or_404(expense_id)
+    form = ExpenseForm(obj=expense)
+
+    if form.validate_on_submit():
+        expense.description = form.description.data
+        expense.amount = float(form.amount.data)
+        db.session.commit()
+        return redirect('/')
+    
+    return render_template('edit.html', form=form)
+
+@main.route('/delete/<int:expense_id>')
+def delete_expense(expense_id):
+    expense = Expense.query.get_or_404(expense_id)
+    db.session.delete(expense)
+    db.session.commit()
+    return redirect('/')
